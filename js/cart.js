11a25d8051c9683f2e7ad2335bcf49a8b0a0ba6b
Man Ping Chen <manpingchen@gmail.com>
2024-07-30 16:15:37 +0100

    增加選項遮罩版型（功能未完成）

--- js/cart.js
+++ js/cart.js
@@ -1,18 +1,26 @@
 function handleAddToCart(element) {
-  if (!element.classList.contains("added")) {
+  const ifProductInCart = element.classList.contains("added");
+  const productId = element.id.split("add-to-cart-")[1];
+  const ifProductCustomizable = document
+    .getElementById(productId)
+    .classList.contains("customizable");
+
+  if (!ifProductInCart) {
     element.classList.add("added");
-    const productId = element.id.split("add-to-cart-")[1];
-    const ifProductCustomizable = document
-      .getElementById(productId)
-      .classList.contains("customizable");
-    console.log(ifProductCustomizable);
+
     if (ifProductCustomizable) {
-      console.log("open OPTIONS overlay");
+      handleShowComponent("#product-options", "flex");
+      document.getElementsByClassName("backdrop")[0].classList.add("gray");
     } else {
       handleShowComponent("#product-added-to-cart", "flex");
       /* update cart*/
       updateCart(productId, 1);
     }
+  }
+
+  if (ifProductInCart && ifProductCustomizable) {
+    handleShowComponent("#product-options", "flex");
+    document.getElementsByClassName("backdrop")[0].classList.add("gray");
   }
 }
 
@@ -22,4 +30,20 @@ function updateCart(productId, amount) {
   [...document.querySelectorAll(".cart-amount")].forEach((o) => {
     o.innerHTML = Number(o.innerHTML) + amount;
   });
+}
+
+function adjustQuantity(actionType) {
+  const valueInput = document.getElementsByClassName("quantity")[0];
+  let value = valueInput.value;
+  let max = 7;
+  actionType === "add" ? value++ : value--;
+  valueInput.value = value;
+
+  document.getElementsByClassName("reduce")[0].disabled = value === 0 ? true : false;
+  validQuantity(value, max);
+}
+
+function validQuantity(value, max) {
+  document.getElementsByClassName("quantity-reach-max")[0].style.display =
+    value === max ? "block" : "none";
 }
