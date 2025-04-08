460a0f54d1a4ab38e083a44d13795410449bda50
Man Ping Chen <manpingchen@gmail.com>
2025-04-08 17:18:16 +0100

    移除css重設指令

--- helpers/iframeAdminController.js
+++ helpers/iframeAdminController.js
@@ -2,8 +2,6 @@ function iframeDisplayController(ifHide) {
   const iframe = document.getElementById("giftAdmin");
   if (ifHide) {
     iframe.style.display = "none";
-    iframe.style.position = "unset";
-    iframe.style.zIndex = "unset";
   } else {
     iframe.style.display = "block";
     iframe.style.position = "absolute";
